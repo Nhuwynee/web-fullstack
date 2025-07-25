ffca71ff08caa57028c385c0f58fe14a2acb492d
Yen Nhu <luungocyennhu810@gmail.com>
2025-07-25 15:45:02 +0700

    updated b2-constructor-accessmodifier

--- java-backend/module2/b2-constructor-accessmodifier/src/TeacherManager.java
+++ java-backend/module2/b2-constructor-accessmodifier/src/TeacherManager.java
@@ -24,7 +24,7 @@ public class TeacherManager {
 
             switch (choose) {
                 case 1:
-                    arrTeacher = addTeacher(arrTeacher);
+                    addTeacher(arrTeacher);
                     System.out.println("Thêm giảng viên thành công!");
                     break;
                 case 2:
